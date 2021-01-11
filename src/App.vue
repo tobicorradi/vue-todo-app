cf0c04bdf252f564a9d9ebefa75a4e65011ed43a
tobicorradi <corraditobias@gmail.com>
2021-01-11 09:56:13 -0300

    Initial Commit

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,73 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="container">
+      <h1>Vue Todo App <span>by Tobias Corradi</span></h1>
+      <div class="search-box">
+        <form @submit="createTask" action="">
+          <input
+            class="search-box__input"
+            type="text"
+            v-model="taskText"
+            placeholder="Write your task here..."
+            autofocus
+          />
+        </form>
+      </div>
+      <div class="list">
+        <div
+          class="list__singleTask"
+          v-for="(task, index) in todoList"
+          v-bind:key="task.text"
+          :class="{ completed: task.isCompleted }"
+          @click="task.isCompleted = !task.isCompleted"
+        >
+          <p>{{ task.text }}</p>
+          <button @click="deleteTask(index)" class="delete-button">
+            Delete
+          </button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  data() {
+    return {
+      taskText: "",
+      todoList: [
+        {
+          text: "¡I am a task!",
+          isCompleted: false,
+        },
+        {
+          text: "Completed Task",
+          isCompleted: true,
+        },
+      ],
+    };
+  },
+  methods: {
+    createTask(e) {
+      e.preventDefault();
+      this.todoList.unshift({ text: this.taskText, isCompleted: false });
+      this.taskText = "";
+    },
+    deleteTask(index) {
+      this.todoList.splice(index, 1);
+    },
+  },
+};
 </script>
 
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -24,5 +75,67 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+h1 {
+  margin-bottom: 20px;
+  font-size: 18px;
+}
+h1 span {
+  font-size: 10px;
+  color: #b9b9b9;
+  font-weight: 200;
+}
+.container {
+  max-width: 400px;
+  margin: 0 auto;
+}
+.search-box {
+  margin-bottom: 30px;
+}
+.search-box__input {
+  width: 100%;
+  padding: 19px 30px;
+  border: 0;
+  box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px;
+  font-size: 15px;
+  border: 1px solid #f6f6f6;
+}
+.search-box__input:focus {
+  outline: 0;
+}
+.list__singleTask {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px;
+  padding: 18px 30px;
+  text-align: left;
+  margin-bottom: 10px;
+  position: relative;
+}
+.list__singleTask p {
+  font-weight: bold;
+  max-width: 203px;
+  word-wrap: break-word;
+  line-height: 21px;
+}
+.completed p {
+  text-decoration: line-through;
+}
+.completed::before {
+  content: "";
+  display: block;
+  width: 3px;
+  height: 100%;
+  background: #05c41c;
+  position: absolute;
+  left: 0;
+}
+button.delete-button {
+  background-color: #eeeeee;
+  border: 0;
+  padding: 10px;
+  border-radius: 4px;
+  color: black;
 }
 </style>
